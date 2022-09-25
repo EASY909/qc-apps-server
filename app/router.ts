96ef33d07aad0338f0308bd4267eaa3eeb6cf9c9
easy909 <1730127479@qq.com>
2022-09-25 20:47:20 +0800

    fix:rename

--- app/router.ts
+++ app/router.ts
@@ -3,5 +3,5 @@ import { Application } from 'egg';
 export default (app: Application) => {
   const { controller, router } = app;
 
-  router.get('/getCashType', controller.cash.getCashType);
+  router.get('/api/getCashType', controller.cash.getCashType);
 };
